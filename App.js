872b8aae75d6d5530c75e1fa4ccbffebfd686e2c
taoufiqq <t.elhanchaoui@gmail.com>
2021-05-04 12:17:03 +0000

    add screenshot

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React ,{ useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './components/Home'
-import CurrencyComponents from './components/currencyTable'
+import HomeScreen from './components/Home';
+import CurrencyComponents from './components/currencyTable';
+import { View, Text } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+
+
   return (
     <NavigationContainer>
     <Stack.Navigator>
@@ -14,5 +42,6 @@ export default function App() {
       <Stack.Screen name="currencyTable" component={CurrencyComponents} />
     </Stack.Navigator>
   </NavigationContainer>
+   
   );
 }
